bf7ee7adf9a3bf2429f4fddf533006fce22de6bd
Raine Alvarez <Raine Alvarez>
2017-02-01 11:51:54 +0800

    Implementing our first route

--- www/js/app.js
+++ www/js/app.js
@@ -22,6 +22,28 @@ angular.module('starter', ['ionic'])
   });
 })
 
+
+.config(function($stateProvider, $urlRouterProvider){
+  $stateProvider
+  .state('tabs', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+  .state('tabs.list', {
+    url: '/list',
+    views:{
+      'list-tab' : {
+        templateUrl: 'templates/list.html',
+        controller: 'ListController'
+      }
+    }
+  })
+
+    $urlRouterProvider.otherwise('/tab/list');
+})
+
+
 .controller('ListController', ['$scope', '$http', function($scope, $http){
   $http.get('js/data.json').success(function(data){
     $scope.artists = data.artists;
