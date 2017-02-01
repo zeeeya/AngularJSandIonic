d928814d5738b164c57bd0dd71c75a8944c83b3b
Raine Alvarez <Raine Alvarez>
2017-02-01 12:54:03 +0800

    creating a calendar view

--- www/js/app.js
+++ www/js/app.js
@@ -59,35 +59,68 @@ angular.module('starter', ['ionic'])
     }
   })
 
+.state('tabs.calendar', {
+    url: '/calendar',
+    views:{
+      'calendar-tab' : {
+        templateUrl: 'templates/calendar.html',
+        controller: 'CalendarController'
+      }
+    }
+  })
+
     $urlRouterProvider.otherwise('/tab/home');
 })
 
+.controller('CalendarController', ['$scope', 
+'$http', '$state',
+    function($scope, $http, $state) {
+    $http.get('js/data.json').success(function(data) {
+      $scope.calendar = data.calendar;
 
-.controller('ListController', ['$scope', '$http', '$state' function($scope, $http, $state){
-  $http.get('js/data.json').success(function(data){
-    $scope.artists = data.artists;
-    $scope.whichartist=$state.params.aId;
-    $scope.data = { showDelete: false; showReorder: false};
+      $scope.onItemDelete = function(dayIndex, item) {
+        $scope.calendar[dayIndex].schedule.splice($scope.calendar[dayIndex].schedule.indexOf(item), 1);
+      }
 
+      $scope.doRefresh =function() {
+      $http.get('js/data.json').success(function(data) {
+          $scope.calendar = data.calendar;
+          $scope.$broadcast('scroll.refreshComplete');
+        });
+      }
 
-$scope.onItemDelete = function(item) {
-  $scope.artists.splice($scope.artists.indexOf(item), l);
-}
+      $scope.toggleStar = function(item) {
+        item.star = !item.star;
+      }
 
-$scope.doRefresh = function(){
-  $http.get('js/data.json').success(function(data){
-    $scope.artists = data.artists;
-    $scope.$broadcast('scroll.refreshComplete');
-  });
-}
+    });
+}])
 
-$scope.toggleStar = function(item){
-  item.star = !item.star;
-}
+.controller('ListController', ['$scope', '$http', '$state',
+    function($scope, $http, $state) {
+    $http.get('js/data.json').success(function(data) {
+      $scope.artists = data.artists;
+      $scope.whichartist=$state.params.aId;
+      $scope.data = { showDelete: false, showReorder: false };
 
-    $scope.moveItem = function(item, fromIndex, toIndex){
-      $scope.artists.splice(fromIndex, l);
-      $scope.artists.splice(toIndex, 0, item);
-    };
-  });
+      $scope.onItemDelete = function(item) {
+        $scope.artists.splice($scope.artists.indexOf(item), 1);
+      }
+
+      $scope.doRefresh =function() {
+      $http.get('js/data.json').success(function(data) {
+          $scope.artists = data;
+          $scope.$broadcast('scroll.refreshComplete'); 
+        });
+      }
+
+      $scope.toggleStar = function(item) {
+        item.star = !item.star;
+      }
+
+      $scope.moveItem = function(item, fromIndex, toIndex) {
+        $scope.artists.splice(fromIndex, 1);
+        $scope.artists.splice(toIndex, 0, item);
+      };
+    });
 }]);
